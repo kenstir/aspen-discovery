4adadb0c6f1482e9f62d5cbae1906d237e75babc
Mark Noble <mdnoble73@gmail.com>
2024-11-10 12:43:49 -0700

    Create new branch

---
+++ code/web/sys/DBMaintenance/version_updates/24.12.00.php
@@ -0,0 +1,35 @@
+<?php
+
+function getUpdates24_12_00(): array {
+	$curTime = time();
+	return [
+		/*'name' => [
+			 'title' => '',
+			 'description' => '',
+			 'continueOnError' => false,
+			 'sql' => [
+				 ''
+			 ]
+		 ], //name*/
+
+		//mark - Grove
+
+		//katherine
+
+		//kirstien
+
+		//kodi
+
+
+		//alexander - PTFS-Europe
+
+		//chloe - PTFS-Europe
+
+
+		//James Staub - Nashville Public Library
+
+
+		//other
+
+	];
+}

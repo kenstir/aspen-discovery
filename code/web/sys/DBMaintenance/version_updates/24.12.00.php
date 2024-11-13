582633a0fdc79f5f9ed9d812e6e8c1d839ff0144
Chloé Zermatten <chloe.zermatten@ptfs-europe.com>
2024-11-13 14:47:34 +0000

    feat: libkey module
    Test plan:
    
    - navigate to Aspen Administration > System
    Administration > Modules
    - on 'LibKey', select 'Edit'
    - toggle 'Enabled' on, and save
    - on page reload, notice that the 'Enabled'
    checkbox remains checked

--- code/web/sys/DBMaintenance/version_updates/24.12.00.php
+++ code/web/sys/DBMaintenance/version_updates/24.12.00.php
@@ -80,7 +80,13 @@ function getUpdates24_12_00(): array {
 		//alexander - PTFS-Europe
 
 		//chloe - PTFS-Europe
-
+		'create_libkey_module' => [
+			'title' => 'Create LibKey Module',
+			'description' => 'Add LibKey to the list of modules',
+			'sql' => [
+				"INSERT INTO modules (name) VALUES ('LibKey')",
+			],
+		], // create_libkey_module
 
 		//James Staub - Nashville Public Library
 

ee929a71046207efab1fa66f2994c36cd9ed37db
Lucas Montoya <lucasmontoya@theke.io>
2024-11-25 13:10:05 +0000

    Improve common entrypoint between services
    - Add a new variable USER_LOCAL_ID
    - Change www-data uid to be equal to the host user's uid.
    - The above mentioned action has been removed from start.sh to be included in the cron entrypoint as well.

--- docker/files/scripts/entrypoint.sh
+++ docker/files/scripts/entrypoint.sh
@@ -1,9 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 if [ "$#" -eq 0 ]; then
+    # Adjust permissions if required
+    if [[ ! -z "${LOCAL_USER_ID}" && "${LOCAL_USER_ID}" != "33" ]]; then
+    	echo "%   Setting www-data to UID=${LOCAL_USER_ID}"
+        usermod -o -u ${LOCAL_USER_ID} "www-data"
+        # Fix permissions due to UID change
+        chown -R "www-data" "/var/log/apache2"
+    fi
+    chown -R www-data /usr/local/aspen-discovery
     exec /start.sh
 elif [ "$1" = 'cron' ]; then
+    # Adjust permissions if required
+    if [[ ! -z "${LOCAL_USER_ID}" && "${LOCAL_USER_ID}" != "33" ]]; then
+    	echo "%   Setting www-data to UID=${LOCAL_USER_ID}"
+        usermod -o -u ${LOCAL_USER_ID} "www-data" 
+    fi
+    chown -R www-data /usr/local/aspen-discovery
     exec /cron.sh
 else
     exec "$@"
